abf16915a1a77628c89a9391ceecc40724e1ff33
Nathan <frost.nathan1995@gmail.com>
2022-06-16 15:47:23 -0400

    Finished queries thru queries_products

---
+++ crud.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('chair', 44.00, 'f'):
+
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('stool', 25.99, 't');
+
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('table', 124.00, 'f');
